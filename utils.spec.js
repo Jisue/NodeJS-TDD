19ab0ca59c1b18ba24a60a8d79f23d5118ac84cb
Jisue <49144667+Jisue@users.noreply.github.com>
2022-12-29 09:42:34 +0900

    add should test

--- utils.spec.js
+++ utils.spec.js
@@ -1,4 +1,5 @@
 // test 코드
+// node 기본 assert 사용
 
 const utils = require('./utils');
 const assert = require('assert');
@@ -7,5 +8,15 @@ describe('utils.js모듈의 capitialize() 함수는 ', () => {
     it('문자열의 첫번째 문자를 대문자로 변환한다', () => {
         const result = utils.capitialize('hello');
         assert.equal(result, 'Hello');
+    })
+})
+
+// should 서드 파티 라이브러리
+const should = require('should');
+
+describe('utils.js모듈의 capitialize() 함수는 ', () => {
+    it('문자열의 첫번째 문자를 대문자로 변환한다', () => {
+        const result = utils.capitialize('hello');
+        result.should.be.equal('Hello');
     })
 })
