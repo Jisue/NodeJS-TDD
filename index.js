d9a5fa04f2646f0f4798bce9bb3adbd5313033bf
Jisue <49144667+Jisue@users.noreply.github.com>
2023-01-01 14:42:33 +0900

    add 사용자 조회 API TEST

--- index.js
+++ index.js
@@ -19,6 +19,19 @@ app.get('/users', function (req, res) {
     res.json(users.slice(0, limit));
 });
 
+app.get('/users/:id', function(req, res) {
+    const id = parseInt(req.params.id);
+
+    // id가 숫자가 아닌 경우 400
+    if(Number.isNaN(id)) return res.status(400).end();
+
+    const user = users.filter((user) => user.id === id)[0];
+    //user를 찾을수 없으면 404
+    if(!user) return res.status(404).end();
+
+    res.json(user);
+})
+
 app.listen(3000, function() {
     console.log('Server is running');
 });
