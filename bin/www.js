041bf5a6405ba130869b66fcb3546c8def298e56
Jisue <49144667+Jisue@users.noreply.github.com>
2023-01-05 20:21:36 +0900

    데이터베이스 - ORM 동기화

--- bin/www.js
+++ bin/www.js
@@ -1,6 +1,10 @@
 // app을 가져와서 서버를 구동
 const app = require('../index');
+const syncDB = require('./sync-db');
 
-app.listen(3000, () => {
-    console.log('Server is running on 3000 port');
-});
+syncDB().then(_=> {
+    console.log('Sync database!');
+    app.listen(3000, () => {
+        console.log('Server is running on 3000 port');
+    });
+})
